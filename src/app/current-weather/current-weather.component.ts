ada7e7d849dc96f0f3d2c29ae412f721a22307a0
Aaron Mitchell <aaronm508@yahoo.com>
2023-03-10 09:43:16 -0600

    mock-up pre-API calls pg.105

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core'
 
+import { ICurrentWeather } from '../interfaces'
+
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.css'],
 })
 export class CurrentWeatherComponent implements OnInit {
-  constructor() {}
+  current: ICurrentWeather
 
+  constructor() {
+    this.current = {
+      city: 'Bethesda',
+      country: 'US',
+      date: new Date(),
+
+      image: '../../assets/img/screenSizes01.ico',
+      temperature: 72,
+      description: 'sunny',
+    } as ICurrentWeather
+  }
   ngOnInit(): void {}
 }
